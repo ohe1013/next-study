f27c8ffacdf2280d19b70ae09178d552a5b23132
ohe1013 <84114149+ohe1013@users.noreply.github.com>
2023-03-07 17:24:30 +0900

    react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { QueryClientProvider, QueryClient, Hydrate } from 'react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
-    <ThemeProvider attribute='class'>
-      <Component {...pageProps} />
+  const queryClient = new QueryClient()
+
+  return (
+    <ThemeProvider attribute="class">
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedProps}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
     </ThemeProvider>
-  ) 
-  
+  )
 }
---
+++ pages/api/menu/[id].tsx
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { TOKEN } from '../../../config'
+import { NextApiRequest, NextApiResponse } from 'next'
+export default async function menu(req: NextApiRequest, res: NextApiResponse) {
+  const { query } = req
+  const { id } = query
+  const { up } = query
+  const options = {
+    method: 'patch',
+    url: `https://api.notion.com/v1/pages/${id}`,
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+    },
+    data: {
+      properties: {
+        up: parseInt(up as string),
+      },
+    },
+  }
+  const _res = await axios.request(options)
+  return await res.status(200).json(_res.data)
+}
