438d70304d5802fd0cf8cf04817eabf86241d786
ohg <dhgusrms23@naver.com>
2023-08-17 00:36:10 +0900

    sdsd

--- pages/api/person/[id].tsx
+++ pages/api/person/[id].tsx
@@ -1,17 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { people } from '../../../data'
 import { Person, ResponseError } from 'pages/person'
+import { TOKEN } from 'config'
+import axios from 'axios'
 
-export default function personHandler(
+export default async function personHandler(
   req: NextApiRequest,
   res: NextApiResponse<Person | ResponseError>,
 ) {
   const { query } = req
   const { id } = query
-  const person = people.find((p) => p.id === id)
-
-  // User with id exists
-  return person
-    ? res.status(200).json(person)
-    : res.status(404).json({ message: `User with id: ${id} not found.` })
+  const options = {
+    method: 'get',
+    url: `https://api.notion.com/v1/pages/${id}`,
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+    },
+  }
+  const _res = await axios.request(options)
+  return await res.status(200).json(_res.data)
 }
