fb1a31b6ac21f6aa514ecc0c26f58c1bc62fffa9
ohhk <ohk@uitgis.com>
2023-09-14 16:35:45 +0900

    setting

--- pages/api/menu/[id].tsx
+++ pages/api/menu/[id].tsx
@@ -2,9 +2,24 @@ import axios from 'axios'
 import { TOKEN } from '../../../config'
 import { NextApiRequest, NextApiResponse } from 'next'
 
+async function getMenu({ id }: { id: any }) {
+  const options = {
+    method: 'get',
+    url: `https://api.notion.com/v1/pages/${id}`,
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+    },
+  }
+  const res = await axios.request(options)
+  return res.data.properties.up.number
+}
+
 export default async function menu(req: NextApiRequest, res: NextApiResponse) {
   const { query } = req
   const { id, up } = query
+  const menu = await getMenu({ id })
   const options = {
     method: 'patch',
     url: `https://api.notion.com/v1/pages/${id}`,
@@ -15,7 +30,7 @@ export default async function menu(req: NextApiRequest, res: NextApiResponse) {
     },
     data: {
       properties: {
-        up: parseInt(up as string),
+        up: +menu + parseInt(up as string),
       },
     },
   }
