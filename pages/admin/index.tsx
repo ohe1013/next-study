ab387a738f38a234f0a6f3df9895961223aa7955
ohhk <ohk@uitgis.com>
2024-01-02 17:32:41 +0900

    add Funnel for admin

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,3 +1,21 @@
+import { useFunnel } from 'src/hooks/useFunnel/useFunnel'
+
 export default function Admin() {
-  return <div className="min-h-body flex-col flex"></div>
+  const [Funnel, setStep] = useFunnel(['init', 'ee'] as const, {
+    initialStep: 'init',
+  })
+  return (
+    <div className="min-h-body flex-col flex">
+      <Funnel>
+        <Funnel.Step name="init">
+          <div>init</div>
+          <button onClick={() => setStep('ee')}>ee</button>
+        </Funnel.Step>
+        <Funnel.Step name="ee">
+          <div>ee</div>
+          <button onClick={() => setStep('init')}>init</button>
+        </Funnel.Step>
+      </Funnel>
+    </div>
+  )
 }
