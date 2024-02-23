5f1d4979c7c3ef476b719ac407d61adb6ed2460d
oh hyun keun <dhgusrms23@naver.com>
2024-02-23 13:59:57 +0900

    fix continuos deployment

---
+++ pages/api/admin/user/databases/index.tsx
@@ -0,0 +1,70 @@
+import axios from 'axios'
+import { DATABASE_ID_ADMININFO, TOKEN } from 'config'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Entries } from 'types/util'
+export default async function adminInfo(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const requestMethod = req.method
+  const properties: any = {}
+  const { page_id } = req.query
+  const createText = (value: string) => [
+    {
+      text: { content: value },
+    },
+  ]
+  ;(Object.entries(req.body) as Entries<Record<string, string>>).forEach(
+    ([key, value]) => {
+      if (key === 'id') {
+        properties[key] = {
+          title: createText(value),
+        }
+      } else {
+        properties[key] = {
+          rich_text: createText(value),
+        }
+      }
+    },
+  )
+  let options
+  switch (requestMethod) {
+    case 'PATCH': {
+      options = {
+        method: requestMethod,
+        url: `https://api.notion.com/v1/pages/${page_id}`,
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          'Notion-Version': '2022-06-28',
+          'content-type': 'application/json',
+        },
+        data: {
+          properties,
+        },
+      }
+      break
+    }
+    default: {
+      options = {
+        method: requestMethod,
+        url: `https://api.notion.com/v1/pages`,
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          'Notion-Version': '2022-06-28',
+          'content-type': 'application/json',
+        },
+        data: {
+          parent: {
+            type: 'database_id',
+            database_id: DATABASE_ID_ADMININFO,
+          },
+          properties,
+        },
+      }
+    }
+  }
+
+  const _res = await axios.request(options)
+
+  return res.status(200).json(_res.data)
+}
